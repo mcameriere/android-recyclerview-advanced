4d761cb34f7528591f46179566181052dd0649fe
mcameriere <31536331+mcameriere@users.noreply.github.com>
2019-09-10 22:37:04 +0200

    Create InvestmentProduct.java

---
+++ InvestmentProduct.java
@@ -0,0 +1,12 @@
+package com.example.investmentproducts;
+
+class InvestmentProduct {
+    String productName;
+    String balance;
+
+    InvestmentProduct(String productName, String balance) {
+        this.productName = productName;
+        this.balance = balance;
+    }
+
+}
